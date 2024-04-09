9da0bfcf95cb654fc2c6251a016b9e9a57b66882
NaitikPatel28 <naitikpatel23125@gmail.com>
2024-04-09 12:55:55 +0530

    abbout

--- saffair-app/src/page/AboutUS/AboutUs.jsx
+++ saffair-app/src/page/AboutUS/AboutUs.jsx
@@ -1,46 +1,50 @@
 import React from "react";
+import Vision from "../../component/homepageComponent/vision mission/Vision";
 
 const AboutUsPage = () => {
   return (
     <div
-      className=" flex flex-col mt-20 sm:mx-20 bg-gray-100 border-2 border-gray-400 mb-10"
+      className=" flex flex-col mt-20 "
       style={{
         borderRadius: "20px",
         fontFamily: "Sans-serif",
-        padding: "40px",
+        paddingTop: "40px",
+        paddingLeft: "25px",
+        paddingRight: "25px",
         color: "black",
       }}
     >
-      <div
+      {/* <div
         className="patch"
         style={{
-          backgroundColor: "#2196BA",
+          // backgroundColor: "#2196BA",
           height: "120px",
           marginBottom: "40px",
+          // padding: "20px",
           borderRadius: "10px",
         }}
       >
-        <div class="logoContainer mt-5 flex justify-center items-center mx-auto">
+         <div class="logoContainer mt-5 flex justify-center items-center mx-auto">
           <img
             class="saffairlogo"
             src="./assets/logofooter.png"
             alt="logo"
             height="50px"
           />
-        </div>
-      </div>
+        </div> 
+      </div> */}
       <br />
-      <h2 className="text-center font-bold sm:text-3xl mb-4">ABOUT US</h2>
+      <h2 className="text-center font-bold sm:text-xl mb-4">ABOUT US</h2>
       <br />
       <h2
-        className="sm:text-4xl"
+        className="sm:text-xl"
         style={{ fontFamily: "Sans-serif", textAlign: "center" }}
       >
         Welcome To <span id="W_Name1">Saffair</span>
       </h2>
       <br />
       <br />
-      <p className="sm:text-2xl ">
+      <p className="sm:text-lg ">
         <span id="W_Name2">Saffair</span> is a Professional{" "}
         <span id="W_Type1">Environmental Awareness Community</span> Platform.
         Here we will only provide you with interesting content that you will
@@ -54,14 +58,14 @@ const AboutUsPage = () => {
         we enjoy giving them to you.
       </p>
       <br />
-      <p className="sm:text-2xl">
+      <p className="sm:text-lg">
         I will keep on posting such valuable and knowledgeable information on my
         Website for all of you. Your love and support matters a lot.
       </p>
       <br />
       <br />
       <p
-        className="sm:text-3xl"
+        className="sm:text-lg"
         style={{ fontWeight: "bold", textAlign: "center" }}
       >
         Thank you For Visiting Our Site
@@ -69,8 +73,13 @@ const AboutUsPage = () => {
         <br />
         <br />
         <span
-          className="sm:text-3xl"
-          style={{ color: "#2196ba", fontWeight: "bold", textAlign: "center" }}
+          className="sm:text-lg"
+          style={{
+            color: "#2196ba",
+            fontWeight: "bold",
+            textAlign: "center",
+            borderBottom: "solid 2px black",
+          }}
         >
           Have A Great Day !
         </span>
@@ -78,6 +87,9 @@ const AboutUsPage = () => {
       <br />
       <br />
       <br />
+      <div>
+        <Vision />
+      </div>
     </div>
   );
 };
